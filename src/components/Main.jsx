061e7289d8142513fc3242ca8ecb5f5e6ecc3e3d
Michele M <mnmin@gmail.com>
2022-07-22 08:38:35 +0100

    Main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -89,20 +89,20 @@ function Main() {
             <h3>How do you rate your rubber duck colour?</h3>
             <ul>
               <li>
-                <input id="color-one" type="radio" name="color" value="1" onChange={duckColor}/><label
-                  for="color-one">1</label>
+                <input id="color-one" type="radio" name="color" value="1" onChange={duckColor}/>
+                <label for="color-one">1</label>
               </li>
               <li>
-                <input id="color-two" type="radio" name="color" value="2" onChange={duckColor}/><label
-                  for="color-two">2</label>
+                <input id="color-two" type="radio" name="color" value="2" onChange={duckColor}/>
+                <label for="color-two">2</label>
               </li>
               <li>
-                <input id="color-three" type="radio" name="color" value="3" onChange={duckColor}/><label
-                  for="color-three">3</label>
+                <input id="color-three" type="radio" name="color" value="3" onChange={duckColor}/>
+                <label for="color-three">3</label>
               </li>
               <li>
-                <input id="color-four" type="radio" name="color" value="4" onChange={duckColor}/><label
-                  for="color-four">4</label>
+                <input id="color-four" type="radio" name="color" value="4" onChange={duckColor}/>
+                <label for="color-four">4</label>
               </li>
             </ul>
           </div>
